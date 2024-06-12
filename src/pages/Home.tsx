30faedf5b6841f3fe6da26b3bdea20e72cac1430
Dariusz Mackowiak <mrdarmack@gmail.com>
2024-06-12 13:13:13 +0200

    add dessert button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,33 +4,34 @@ import RecipeComponent from "./components/RecipeComponent";
 type Type = "BREAKFAST" | "DINNER" | "LUNCH";
 
 const Home: React.FC = () => {
-  const [buttonClicked, setButtonClicked] = useState<boolean>(false);
-  const [typeProp, setTypeProp] = useState<Type | undefined>(undefined);
+    const [buttonClicked, setButtonClicked] = useState<boolean>(false);
+    const [typeProp, setTypeProp] = useState<Type | undefined>(undefined);
 
-  const handleBreakfastClick = () => {
-    setTypeProp("BREAKFAST");
-    setButtonClicked(true);
-  };
+    const handleBreakfastClick = () => {
+        setTypeProp("BREAKFAST");
+        setButtonClicked(true);
+    };
 
-  const handleDinnerClick = () => {
-    setTypeProp("DINNER");
-    setButtonClicked(true);
-  };
+    const handleDinnerClick = () => {
+        setTypeProp("DINNER");
+        setButtonClicked(true);
+    };
 
-  const handleLunchClick = () => {
-    setTypeProp("LUNCH");
-    setButtonClicked(true);
-  };
+    const handleLunchClick = () => {
+        setTypeProp("LUNCH");
+        setButtonClicked(true);
+    };
 
-  return (
-    <div>
-      <h1>AEH TOP PROJEKT</h1>
-      <button onClick={handleBreakfastClick}>BREAKFAST</button>
-      <button onClick={handleDinnerClick}>DINNER</button>
-      <button onClick={handleLunchClick}>LUNCH</button>
-      {buttonClicked && <RecipeComponent type={typeProp} />}
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <h1>AEH TOP PROJEKT</h1>
+            <button onClick={handleBreakfastClick}>BREAKFAST</button>
+            <button onClick={handleDinnerClick}>DINNER</button>
+            <button onClick={handleLunchClick}>LUNCH</button>
+            <button>DESSERT</button>
+            {buttonClicked && <RecipeComponent type={typeProp} />}
+        </div>
+    );
 };
 
 export default Home;
