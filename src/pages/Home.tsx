9a5b3e3795b5aeb117cfcf01176f6ef579691517
wiertlooo <watulewicz@gmail.com>
2024-06-15 21:31:54 +0200

    s

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,47 +6,42 @@ import { recipesApi } from "../store/apis/recipesApi";
 type Type = "BREAKFAST" | "DINNER" | "LUNCH" | "DESSERT";
 
 const Home: React.FC = () => {
-    const [typeProp, setTypeProp] = useState<Type | undefined>(undefined);
-    const dispatch = useDispatch();
+  const [typeProp, setTypeProp] = useState<Type | undefined>(undefined);
+  const dispatch = useDispatch();
 
-    const handleClick = (type: Type) => {
-        setTypeProp(type);
-        dispatch(
-            recipesApi.util.invalidateTags([{ type: "Recipe", id: type }])
-        );
-    };
+  const handleClick = (type: Type) => {
+    setTypeProp(type);
+    dispatch(recipesApi.util.invalidateTags([{ type: "Recipe", id: type }]));
+  };
 
-    return (
-        <div className="wrapper">
-            <h1>Recipe for...</h1>
-            <div className="btn-container">
-                <button
-                    className="btn btn-breakfast"
-                    onClick={() => handleClick("BREAKFAST")}
-                >
-                    Breakfast
-                </button>
-                <button
-                    className="btn btn-dinner"
-                    onClick={() => handleClick("DINNER")}
-                >
-                    Dinner
-                </button>
-                <button
-                    className="btn btn-supper"
-                    onClick={() => handleClick("LUNCH")}
-                >
-                    Supper
-                </button>
-                <button
-                    className="btn btn-dessert"
-                    onClick={() => handleClick("DESSERT")}
-                >
-                    Dessert
-                </button>
-            </div>
-            {typeProp && <RecipeComponent type={typeProp} />}
-            <div id="recipe-container">
+  return (
+    <div className="wrapper">
+      <h1>Recipe for...</h1>
+      <div className="btn-container">
+        <button
+          className="btn btn-breakfast"
+          onClick={() => handleClick("BREAKFAST")}
+        >
+          Breakfast
+        </button>
+        <button
+          className="btn btn-dinner"
+          onClick={() => handleClick("DINNER")}
+        >
+          Dinner
+        </button>
+        <button className="btn btn-supper" onClick={() => handleClick("LUNCH")}>
+          Supper
+        </button>
+        <button
+          className="btn btn-dessert"
+          onClick={() => handleClick("DESSERT")}
+        >
+          Dessert
+        </button>
+      </div>
+      {typeProp && <RecipeComponent type={typeProp} />}
+      {/* <div id="recipe-container">
                 <div className="two-column">
                     <div className="col-left">
                         <h2>Green fantasy</h2>
@@ -85,9 +80,9 @@ const Home: React.FC = () => {
                     a batter. Pour over sausages and bake until puffed and
                     golden.
                 </p>
-            </div>
-        </div>
-    );
+            </div> */}
+    </div>
+  );
 };
 
 export default Home;
